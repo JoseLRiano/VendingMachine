120681c2517f954b23bbc4264c982efe0a0a9c51
JoseLRiano <joselriano@yahoo.com>
2020-06-06 02:46:00 -0400

    Updates

---
+++ precache-manifest.ed58a4c13b35b8f994842264651bc620.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6dcdfe8f9edebeac399c0897bef807b",
+    "url": "/VendingMachine/index.html"
+  },
+  {
+    "revision": "d51131528324ae8ad29f",
+    "url": "/VendingMachine/static/css/main.fa85d73c.chunk.css"
+  },
+  {
+    "revision": "bf8ce5e5af2ce83a7b0b",
+    "url": "/VendingMachine/static/js/2.8eab3a36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/VendingMachine/static/js/2.8eab3a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51131528324ae8ad29f",
+    "url": "/VendingMachine/static/js/main.8f9841e1.chunk.js"
+  },
+  {
+    "revision": "4351045ca59c9d4467ae",
+    "url": "/VendingMachine/static/js/runtime-main.cd5879bc.js"
+  }
+]);
